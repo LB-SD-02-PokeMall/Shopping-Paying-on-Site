a5b9b2f8c70654c1e24caf0e71ccf4c1ee035121
hannahyvonne16 <hannahyvonne16@gmail.com>
2022-08-10 05:54:25 -0700

    merging forked repos into one, adding all paths, styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,31 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductList from "./pages/ProductsPage";
 import Homepage from './pages/Homepage'
-import Product from './pages/Product'
-
+import ProductDetails from "./pages/DetailsPage"
+import AboutUs from "./pages/AboutUsPage";
+import ViewCart from "./pages/ViewCart";
+import AccessoriesPage from "./pages/AccessoriesPage";
+import AccessoryDetailsPage from "./pages/AccessoryDetailsPage";
+import './App.css'
+ 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-      <Routes>
-        <Route path='/home' element={<Homepage/>} />
-        <Route path='/product' element={<Product/>} />
-      </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
-
+ return (
+   <div className="main-page">
+     <BrowserRouter>
+     <Routes>
+       <Route exact path="/" element={<Homepage />} />
+       <Route exact path='/home' element={<Homepage/>} />
+       <Route exact path='/aboutus' element={<AboutUs/>} />
+       <Route exact path='/product' element={<ProductList/>} />
+       <Route exact path="/product/:id" element={<ProductDetails/>}/>
+       <Route exact path='/accessories' element={<AccessoriesPage/>} />
+       <Route exact path='/accessories/:id' element={<AccessoryDetailsPage/>} />    
+       <Route exact path='/cart' element={<ViewCart/>} />   
+     </Routes>
+     </BrowserRouter>
+   </div>
+ );
+};
+ 
 export default App;
